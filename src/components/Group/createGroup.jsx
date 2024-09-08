9b58dda870cae2d277a98dc84ecb4463dedff8f7
Haiderbukhari <apshaiderbukhari786@gmail.com>
2024-09-09 01:12:18 +0500

    groups

---
+++ src/components/Group/createGroup.jsx
@@ -0,0 +1,115 @@
+import axios from 'axios';
+import { useState, useRef, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { successToast } from '../../utils/toastNotifications';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+const CreateGroup = ({ open, setOpen, submit, setSubmit }) => {
+    const [userData, setUserData] = useState({
+        name: '',
+        contactNumber: '',
+        email: '',
+        talksAbout: '',
+        description: ''
+    });
+    const [tempUserData, setTempUserData] = useState({});
+    const [profilePhoto, setProfilePhoto] = useState(null);
+    const fileInputRef = useRef(null);
+    const token = useSelector(state => state.profile.jwt);
+    const id = useSelector(state => state.profile.id);
+    const dispatch = useDispatch();
+    const Navigate = useNavigate();
+
+    const handleClose = () => {
+        setOpen(!open);
+        setUserData({
+            name: '',
+            contactNumber: '',
+            email: '',
+            talksAbout: '',
+            description: ''
+        });
+    }
+
+    const handlePhotoChange = (event) => {
+        setProfilePhoto(event.target.files[0]);
+    };
+
+    const handlePhotoClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleSubmit = async () => {
+        const formData = new FormData();
+        formData.append('file', profilePhoto);
+        formData.append('name', userData.name);
+        formData.append('contactNumber', userData.contactNumber);
+        formData.append('email', userData.email);
+        formData.append('talksAbout', userData.talksAbout);
+        formData.append('description', userData.description);
+        try {
+            await axios.post(`${import.meta.env.VITE_APP_BACKEND_URL}/groups`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': `Bearer ${token}`
+                }
+            }).then(() => {
+                successToast("Group Created Successfully");
+                handleClose();
+                setSubmit(false)
+            })
+        } catch (error) {
+            console.error('Error uploading file:', error);
+            setSubmit(false)
+        }
+    };
+
+    useEffect(() => {
+        if (submit) {
+            handleSubmit();
+        }
+    }, [submit])
+
+    return (
+        <div>
+            <div
+                className="w-full border border-solid aspect-[9.09] max-w-[447px] stroke-[1px] bg-[#AAAAAA] bg-opacity-40 rounded-2xl px-3 py-3 cursor-pointer"
+                onClick={handlePhotoClick}
+            >
+                <div className="flex gap-2 text-lg font-bold leading-4 text-violet-800 whitespace-nowrap">
+                    <img
+                        loading="lazy"
+                        src={profilePhoto ? URL.createObjectURL(profilePhoto) : "https://cdn.builder.io/api/v1/image/assets/TEMP/92a2157aee4e429aae8e4dbeb9e67d66034defda4c489409e9a2e3627dc8db0e?"}
+                        className="shrink-0 aspect-square w-[23px]"
+                        alt="Profile"
+                    />
+                    <div className="my-auto text-[17px] ml-0">Group Picture</div>
+                </div>
+            </div>
+            <input
+                type="file"
+                accept="image/*"
+                onChange={handlePhotoChange}
+                className="hidden"
+                ref={fileInputRef}
+            />
+            <div className='w-[80%] mt-5'>
+                <div className="flex flex-col text-base font-medium tracking-wider leading-4 max-w-[361px] text-neutral-800">
+                    <div className="w-full font-semibold">Group Name</div>
+                    <input onChange={(event) => { setUserData({ ...userData, name: event.target.value }) }} value={userData?.name ?? ""} style={{ border: "1px solid #AAAAAA" }} type="text" name="" id="" className="justify-center items-start px-5 py-2 mt-3 w-full text-base tracking-wider rounded-xl border border-solid outline-none text-black bg-zinc-300 border-stone-300" placeholder='i.e. jhon' />
+                    <div className="mt-6 w-full font-semibold">Contact Number</div>
+                    <input onChange={(event) => { setUserData({ ...userData, contactNumber: event.target.value }) }} value={userData?.contactNumber ?? ""} style={{ border: "1px solid #AAAAAA" }} type="text" name="" id="" className="justify-center items-start px-5 py-2 mt-3 w-full text-base tracking-wider rounded-xl border border-solid outline-none text-black bg-zinc-300 border-stone-300" placeholder='i.e. bravo' />
+                    <div className="mt-6 w-full font-semibold">Email</div>
+                    <input onChange={(event) => { setUserData({ ...userData, email: event.target.value }) }} value={userData?.email ?? ""} style={{ border: "1px solid #AAAAAA" }} type="text" name="" id="" className="justify-center items-start px-5 py-2 mt-3 w-full text-base tracking-wider rounded-xl border border-solid outline-none text-black bg-zinc-300 border-stone-300" placeholder='i.e. bravo' />
+                    <div className="mt-5 w-full font-semibold">Talks About</div>
+                    <textarea onChange={(event) => { setUserData({ ...userData, talksAbout: event.target.value }) }} value={userData?.talksAbout ?? ""} rows={4} style={{ border: "1px solid #AAAAAA" }} className="justify-center items-start px-5 py-2 mt-3 w-full text-base tracking-wider rounded-xl border border-solid outline-none text-black bg-zinc-300 border-stone-300" placeholder='Type here' name="" id=""></textarea>
+                    <div className="mt-5 w-full font-semibold">Description</div>
+                    <textarea onChange={(event) => { setUserData({ ...userData, description: event.target.value }) }} value={userData?.description ?? ""} rows={4} style={{ border: "1px solid #AAAAAA" }} className="justify-center items-start px-5 py-2 mt-3 w-full text-base tracking-wider rounded-xl border border-solid outline-none text-black bg-zinc-300 border-stone-300" placeholder='Type here' name="" id=""></textarea>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CreateGroup;
--- src/pages/Groups/index.jsx
+++ src/pages/Groups/index.jsx
@@ -1,30 +1,69 @@
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import CreateGroup from "../../components/Group/createGroup";
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { failedToast } from "../../utils/toastNotifications";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 export default function Groups() {
+
     const text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
     const Navigate = useNavigate();
+
+    const [open, setOpen] = useState(false)
+    const [submit, setSubmit] = useState(false)
+    const [data, setData] = useState([])
+    const token = useSelector(state => state.profile.jwt);
+
+    async function getAllGroupData() {
+        await axios.get(`${import.meta.env.VITE_APP_BACKEND_URL}/groups`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        }).then((Item) => {
+            setData(Item.data.data);
+            console.log(Item.data.data)
+        }).catch((err) => {
+            return failedToast(err.response.data.error);
+        });
+    }
+    useEffect(() => {
+        getAllGroupData();
+    }, [open])
+
     return (
         <div className="flex flex-col px-5 max-w-[99%] ">
-            <div className="mt-14 w-full text-xl lg:text-2xl font-semibold text-zinc-500 max-md:mt-10 max-md:max-w-full">
-                Entrepreneur Groups{" "}
+            <div className="flex justify-between items-center max-md:mt-10 mt-14 ">
+                <div className="w-full text-xl lg:text-2xl font-semibold text-zinc-500 max-md:max-w-full">
+                    Groups{" "}
+                </div>
+
+                <button
+                    onClick={() => { setOpen(!open) }}
+                    className="inline-flex items-center justify-center px-6 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-violet-800 border border-transparent hover:bg-violet-700 focus:outline-none focus:ring-offset-2 focus:ring-violet-700 rounded-2xl w-[200px]"
+                >
+                    Add New Group
+                </button>
             </div>
             {
-                new Array(10).fill(0).map((_, i) => (
+                data?.map((item, i) => (
                     <div key={i} className="px-5 2xl:px-7 py-5 mt-5 w-full rounded-3xl border border-solid bg-neutral-200 border-neutral-400 max-md:px-3 max-md:py-3 max-md:max-w-full">
                         <div className="flex gap-5 max-md:flex-row max-md:gap-0 justify-between max-2xl:block max-md:flex">
                             <div className="flex flex-col max-md:ml-0 max-md:w-full">
                                 <div className="flex grow gap-4 max-md:gap-2 max-md:mt-0 max-md:items-center">
                                     <img
                                         loading="lazy"
-                                        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/38c5b4735a899f8e1f0330e48ffd4f7e6b5350d54ab094cd0066d3ec939cb22c?apiKey=cf358c329e0d49a792d02d32277323ef&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/38c5b4735a899f8e1f0330e48ffd4f7e6b5350d54ab094cd0066d3ec939cb22c?apiKey=cf358c329e0d49a792d02d32277323ef&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/38c5b4735a899f8e1f0330e48ffd4f7e6b5350d54ab094cd0066d3ec939cb22c?apiKey=cf358c329e0d49a792d02d32277323ef&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/38c5b4735a899f8e1f0330e48ffd4f7e6b5350d54ab094cd0066d3ec939cb22c?apiKey=cf358c329e0d49a792d02d32277323ef&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/38c5b4735a899f8e1f0330e48ffd4f7e6b5350d54ab094cd0066d3ec939cb22c?apiKey=cf358c329e0d49a792d02d32277323ef&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/38c5b4735a899f8e1f0330e48ffd4f7e6b5350d54ab094cd0066d3ec939cb22c?apiKey=cf358c329e0d49a792d02d32277323ef&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/38c5b4735a899f8e1f0330e48ffd4f7e6b5350d54ab094cd0066d3ec939cb22c?apiKey=cf358c329e0d49a792d02d32277323ef&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/38c5b4735a899f8e1f0330e48ffd4f7e6b5350d54ab094cd0066d3ec939cb22c?apiKey=cf358c329e0d49a792d02d32277323ef&"
+                                        src={item.groupImage}
                                         className="shrink-0 aspect-square h-[40px] w-[40px] md:h-[50px] md:w-[50px] border-4 rounded-xl border-violet-800 border-solid"
                                     />
                                     <div className="flex flex-col py-1 my-auto max-md:my-0">
                                         <div className="text-base max-md:text-sm font-medium text-zinc-800">
-                                            Group Name Here
+                                            {item.name}
                                         </div>
                                         <div className="text-xs text-ellipsis text-neutral-500 max-md:hidden">
-                                            {text.length > 60 ? text.slice(0, 60) + '...' : text}
+                                            {item.description > 60 ? item.description.slice(0, 60) + '...' : item.description}
                                         </div>
                                     </div>
                                 </div>
@@ -37,9 +76,9 @@ export default function Groups() {
                                             src="https://cdn.builder.io/api/v1/image/assets/TEMP/cbf9f452b2ad0d9bc2072c788e3cdac85906c6c3a63ab0a7ec8669c8d70dc7de?apiKey=cf358c329e0d49a792d02d32277323ef&"
                                             className="shrink-0 aspect-square w-[25px] max-md:w-[15px]"
                                         />
-                                        <div className="my-auto">27k</div>
+                                        <div className="my-auto">{item.likes}</div>
                                     </div>
-                                    <div onClick={()=>{Navigate('/dashboard/details/groups')}} className="flex justify-center px-5 py-1 text-base xl:text-lg text-white bg-violet-800 rounded-xl border border-solid w-full max-w-[130px] xl:max-w-[130px] border-neutral-400 max-md:px-3 max-md:py-2 max-md:max-w-[80px] max-md:text-xs">
+                                    <div onClick={() => { Navigate(`/dashboard/details/groups?id=${item._id}`) }} className="flex justify-center px-5 py-1 text-base xl:text-lg text-white bg-violet-800 rounded-xl border border-solid w-full max-w-[130px] xl:max-w-[130px] border-neutral-400 max-md:px-3 max-md:py-2 max-md:max-w-[80px] max-md:text-xs cursor-pointer">
                                         Join Now
                                     </div>
                                 </div>
@@ -56,6 +95,43 @@ export default function Groups() {
                 src="https://cdn.builder.io/api/v1/image/assets/TEMP/921125f176c766753f5022d43c5fc218a48df3d917ea9d45a70550fa14d9058a?apiKey=cf358c329e0d49a792d02d32277323ef&"
                 className="self-center mt-2 mb-4 border-solid aspect-[8.33] border-[3px] rounded-xl border-zinc-600 stroke-[5px] stroke-zinc-600 w-[43px]"
             />
+
+            <React.Fragment>
+                <Dialog
+                    open={open}
+                    onClose={()=>{setOpen(false)}}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                    PaperProps={{
+                        sx: {
+                            backgroundColor: '#E0E0E0'
+                        }
+                    }}
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        <p className='font-semibold'>
+                            {"Create Group"}
+                        </p>
+                        <div className='mx-3 my-2' style={{ borderBottom: "2px solid #ccc" }}></div>
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description" sx={{width: "300px"}}>
+                            {open && <CreateGroup open={open} setOpen={setOpen} submit={submit} setSubmit={setSubmit} />}
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                    <div className='w-[290px] md:w-[400px] mb-3'>
+                        <div className='mx-3 my-2 mb-5' style={{ borderBottom: "2px solid #ccc" }}></div>
+                        <div className='mx-4'>
+                            <div className="flex w-full justify-between">
+                                <button onClick={()=>{setOpen(!open)}} className='w-[140px] px-10 py-2 rounded-xl text-center text-black bg-[#CCCCCC]'>Cancel</button>
+                                <button onClick={()=>{setSubmit(!submit)}} className='w-[140px] px-10 py-2 rounded-xl text-center text-white bg-[#4C1DBE]'>Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </DialogActions>
+                </Dialog>
+            </React.Fragment>
         </div>
     );
 }
