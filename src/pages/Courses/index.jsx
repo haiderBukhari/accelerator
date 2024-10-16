505bbcb14db67f776ee7ef120e4fa16b3b47e35d
Haiderbukhari <apshaiderbukhari786@gmail.com>
2024-10-16 20:36:33 +0500

    group id fixed

--- src/pages/Courses/index.jsx
+++ src/pages/Courses/index.jsx
@@ -96,9 +96,9 @@ export default function Courses({groupId}) {
                                 {
                                     userData.isAdmin &&
                                     <div className="w-full mx-auto flex justify-center items-center my-9">
-                                        <button onClick={() => { Navigate(`/dashboard/modules?title=${Item.name}&groupId=${groupId}`) }} className="inline-flex items-center justify-center px-6 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-red-800 border border-transparent hover:bg-red-700 focus:outline-none focus:ring-offset-2 focus:ring-red-700 rounded-2xl hover:opacity-80">Add New Module</button>
+                                        <button onClick={() => { Navigate(`/dashboard/modules?title=${Item.name}&${groupId ? `groupId=${groupId ?? ''}` : ''}`) }} className="inline-flex items-center justify-center px-6 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-red-800 border border-transparent hover:bg-red-700 focus:outline-none focus:ring-offset-2 focus:ring-red-700 rounded-2xl hover:opacity-80">Add New Module</button>
 
-                                        <button onClick={() => { Navigate(`/dashboard/quiz?title=${Item.name}&groupId=${groupId}`) }} className="inline-flex items-center justify-center px-6 py-2 text-sm ml-5 font-medium leading-5 text-white transition duration-150 ease-in-out bg-red-800 border border-transparent hover:bg-red-700 focus:outline-none focus:ring-offset-2 focus:ring-red-700 rounded-2xl hover:opacity-80">Add New Quiz</button>
+                                        <button onClick={() => { Navigate(`/dashboard/quiz?title=${Item.name}&${groupId ? `groupId=${groupId ?? ''}` : ''}`) }} className="inline-flex items-center justify-center px-6 py-2 text-sm ml-5 font-medium leading-5 text-white transition duration-150 ease-in-out bg-red-800 border border-transparent hover:bg-red-700 focus:outline-none focus:ring-offset-2 focus:ring-red-700 rounded-2xl hover:opacity-80">Add New Quiz</button>
                                     </div>
                                 }
                             </div>
